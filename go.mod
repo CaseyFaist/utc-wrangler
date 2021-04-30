9c405eeafa872a6ff9a0051d80420dcbd747b0da
caseyfaist <caseylfaist@gmail.com>
2021-04-30 18:20:13 -0400

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/caseyfaist/utc-wrangler
+
+go 1.16
+
+require (
+	github.com/araddon/dateparse v0.0.0-20210429162001-6b43995a97de
+	github.com/davecgh/go-spew v1.1.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/araddon/dateparse"
+)
+
+// TODO:
+// Output Formats:
+//
+// Standard output
+// Jira output
+//
+// Converted to:
+// My Local
+// UTC
+
+var (
+	timezone = ""
+	date     = time.Now()
+)
+
+func useZone(datestring string, zone string) (date time.Time) {
+
+	tz, err := time.LoadLocation(zone)
+	if err != nil {
+		panic(err.Error())
+	}
+	time.Local = tz
+
+	date, err = dateparse.ParseIn(datestring, time.Local)
+	if err != nil {
+		panic(err.Error())
+	}
+	return
+}
+
+func useUTC(datestring string) (date time.Time) {
+	date, err := dateparse.ParseAny(datestring)
+	if err != nil {
+		panic(err.Error())
+	}
+	return
+}
+
+func main() {
+
+	flag.StringVar(&timezone, "timezone", "UTC", "Timezone aka `America/Los_Angeles` formatted time-zone")
+	convertToLocal := flag.Bool("convertToLocal", false, "Parse output to your local time")
+	flag.Parse()
+
+	args := flag.Args()
+	fullstr := strings.Join(args, " ")
+	fmt.Println("Input:")
+	fmt.Println(args)
+
+	if timezone != "UTC" {
+		date = useZone(fullstr, timezone).UTC()
+	} else {
+		date = useUTC(fullstr)
+	}
+
+	if *convertToLocal {
+		fmt.Println(date.Local().String())
+	} else {
+		fmt.Println(date.String())
+	}
+}
